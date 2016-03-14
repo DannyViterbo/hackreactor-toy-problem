b479d04feb32862d0d9b75729dc17381a582239e
pedrotp <pedro@pedrotp.com>
2016-03-14 14:48:36 -0400

    refactored prime tester and extra credit into one file

--- solutions/prime_tester.js
+++ solutions/prime_tester.js
@@ -1,6 +1,4 @@
 function primeTester (n) {
-  // Write your code here, and
-  // return your final answer.
   if (n === 1) { return false; }
   for (var i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
@@ -25,3 +23,52 @@ function primeTester (n) {
 
 // input: 1
 // output: false
+
+
+function primeSieve (start, end) {
+  var store = {};
+  function markMultiples(num) {
+    var mul = 2;
+    while (mul*num <= end) { 
+     store[mul*num] = true;
+     mul++;
+    }
+  }
+  for (var i = 2; i <= end; i++) {
+    if (!store[i]) {
+      markMultiples(i);
+    }
+  }
+  var primes = [];
+  for (var j = start; j <= end; j++) {
+    if (j < 2) continue;
+    if (!store[j]) {
+      primes.push(j)
+    }
+  }
+  return primes;
+};
+
+// Extra credit:
+
+// Write a function that generates a list of all prime numbers in a user-specified range (inclusive). If you're not quite sure where to start, check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling saucy, check out the Sieve of Atkin.) 
+
+// Parameters:
+
+// start > begin of range
+
+// end > end of range (inlcusive)
+
+// Examples
+
+// start: 20
+// end: 22
+// output: [ ]
+
+// start: 1
+// end: 2
+// output: [ 2 ]
+
+// start: 16
+// end: 20
+// output: [ 17, 19 ]
